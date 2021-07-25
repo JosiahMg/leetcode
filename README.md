d018b0b3214bbf7c10326041331f1f33e67ef678
JosiahMg <JosiahMg@163.com>
2021-07-25 13:16:24 +0800

    binary search

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# leetcode
+# leetcode one by one
 
 ### 1. [Two Sum](https://leetcode-cn.com/problems/two-sum)
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
 
-### 2. [Add Two Numbers](https://leetcode-cn.com/problems/add-two-numbers)
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+
+
+### 2. [Add Two Numbers](https://leetcode-cn.com/problems/add-two-numbers)  
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+
+
+***
+
+# 二分查找
+### 704. [Binary Search](https://leetcode-cn.com/problems/binary-search)
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 
 
 
---
+++ python/704.Binary Search.py
@@ -0,0 +1,46 @@
+"""
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-search
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            pivot = left + (right-left)//2
+            if nums[pivot] == target:
+                return pivot
+            if nums[pivot] < target:
+                left = pivot +1
+            else:
+                right = pivot - 1
+        return -1
+
+s = Solution()
+
+nums = [-1,0,3,5,9,12]
+target = 2
+
+print(s.search(nums, target))
+
